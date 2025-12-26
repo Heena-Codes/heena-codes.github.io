9af5e374488c2a59e787301e654bf88b21c27612
Heena Gupta <heenamahour984@gmail.com>
2025-12-26 17:24:51 +0530

    Updates

--- src/routes/Biography.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { StyledNavLink } from '../components/styled-components/route'
-
-export const BiographyProfile = () => {
-    return <StyledNavLink to="/contributions">Contributions</StyledNavLink>
-}
--- src/routes/LinkedIn.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { StyledLink } from '../components/styled-components/route'
-
-export const LinkedInProfile = () => {
-    return <StyledLink to="https://www.linkedin.com/in/heena-gupta-a183ba99/" target="_blank">LinkedIn</StyledLink>
-}
